c44d2c7ae848e37f0f72a8182d34f5afd16a3b0c
Omran Khoja <okhoja@kwi.com>
2023-03-08 16:45:43 -0500

    Updates to use the same module name as the original cocoapods implementation

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "GCDWebServers",
-            targets: ["GCDWebServers"]),
+            name: "GCDWebServer",
+            targets: ["GCDWebServer"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "GCDWebServers",
+            name: "GCDWebServer",
             dependencies: [],
             path: ".",
             exclude: [
@@ -44,15 +44,15 @@ let package = Package(
                 .headerSearchPath("GCDWebServer/Responses"),
             ]
         ),
-        .testTarget(
-            name: "GCDWebServersTests",
-            dependencies: ["GCDWebServers"],
-            path: "Frameworks",
-            exclude: [
-                "GCDWebServers.h",
-                "Info.plist",
-                "module.modulemap"
-            ]
-        ),
+//        .testTarget(
+//            name: "GCDWebServersTests",
+//            dependencies: ["GCDWebServers"],
+//            path: "Frameworks",
+//            exclude: [
+//                "GCDWebServers.h",
+//                "Info.plist",
+//                "module.modulemap"
+//            ]
+//        ),
     ]
 )
